d3486f3dcc8d91768d0d4607fb890395a2bc2123
shreyakale08 <kaleshreya347@gmail.com>
2026-06-02 00:02:49 +0530

    Add Empty String Exception program

---
+++ src/practiceException/EmptyStringException.java
@@ -0,0 +1,10 @@
+package practiceException;
+
+@SuppressWarnings("serial")
+public class EmptyStringException extends Exception{
+ 
+	public  EmptyStringException(String message){
+		
+		super(message);
+	}
+}
